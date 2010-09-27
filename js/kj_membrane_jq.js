e719a550326c5d2ded1cc9b60082a72d6cabade4
DouG Molidor <doug@creativelift.net>
2010-09-27 04:30:46 -0700

    Tweaked some CSS and JS after adding in Yes/No option

--- js/kj_membrane_jq.js
+++ js/kj_membrane_jq.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
-	$("#formFieldEl1").val("");
+	$("#formFieldEl2").val("");
 	$(".fieldRequiredStar").html("");
-	
+	$("#formField3 .optionRow:eq(1)").append("<span class='fieldHelp'>" + $("#formField3Subtitle").text() + "</span>");
+	$("#formField3Subtitle").remove();
 });
